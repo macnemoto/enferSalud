9a546ff176c1a2ee382e69be96ecfa95af8aa809
macnemoto <marcosj465@gmail.com>
2025-11-07 22:24:44 -0400

    code

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <img src="/resources/logo.png" alt="logoEmpresa" />
+        <p className='text-[#f1be23] text-center mb-28'>Seguimiento, Control y Análisis asistencial de pacientes</p>
+        <p className='text-[#dbdbdb] text-2xl'>Introduzca sus datos de acceso a Enfersalud</p>
+      </div>
+    </>
+  )
+}
+
+export default App
