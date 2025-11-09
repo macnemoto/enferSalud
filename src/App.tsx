b578386cfce3fe6b3b9abed4d9487203ee83001f
macnemoto <marcosj465@gmail.com>
2025-11-09 18:58:48 -0400

    Login screen improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import  { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import type { SubmitHandler } from 'react-hook-form'
 
 interface LoginFormInputs {
@@ -22,15 +22,14 @@ function App() {
         <p className='text-[#f1be23] text-center mb-28'>Seguimiento, Control y Análisis asistencial de pacientes</p>
         <p className='text-[#dbdbdb] text-2xl text-center'>Introduzca sus datos de acceso a Enfersalud</p>
       </div>
-      <form className='flex flex-col' onSubmit={handleSubmit(myFuncionTheSumit)}>
-        <label htmlFor="">Usuario:</label>
-        <input className='bg-amber-50' type="text" {...register('usuario')} />
-        <label htmlFor="">password</label>
-        <input className='bg-amber-50' type="password" {...register('password', { required: 'La contraseña es obligatoria' })} />
-        {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
-        <button type='submit'>Iniciar Sesión</button>
+      <form className='flex flex-col text-[#f1be23]' onSubmit={handleSubmit(myFuncionTheSumit)}>
+        <label className='mb-2' htmlFor="">Usuario</label>
+        <input className='bg-amber-50 rounded-xl mb-2 text-black py-2 pl-4' type="text" {...register('usuario')} />
+        <label className='mb-2' htmlFor="">Password</label>
+        <input className='bg-amber-50 rounded-xl text-black py-2 pl-4' type="password" {...register('password', { required: 'La contraseña es obligatoria' })} />
+        {errors.password && <p className='my-5' style={{ color: 'red' }}>{errors.password.message}</p>}
+        <button className='mt-10 text-white bg-amber-500 rounded-xl p-4' type='submit'>Iniciar Sesión</button>
       </form>
-      {/* <button type='submit'>Iniciar Sesión</button> */}
     </>
   )
 }
